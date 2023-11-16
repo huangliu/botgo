4b6c7bf6c036c03ad481bd4c32ceef54b217bd0f
huangliu <huangliu3060@qq.com>
2023-11-16 16:12:53 +0800

    dto: 修改markdown的模版ID类型和名称

--- dto/message_create.go
+++ dto/message_create.go
@@ -24,9 +24,9 @@ type MessageReference struct {
 
 // Markdown markdown 消息
 type Markdown struct {
-	TemplateID int               `json:"template_id"` // 模版 id
-	Params     []*MarkdownParams `json:"params"`      // 模版参数
-	Content    string            `json:"content"`     // 原生 markdown
+	CustomTemplateID string            `json:"custom_template_id"` // 模版 id
+	Params           []*MarkdownParams `json:"params"`             // 模版参数
+	Content          string            `json:"content"`            // 原生 markdown
 }
 
 // MarkdownParams markdown 模版参数 键值对
